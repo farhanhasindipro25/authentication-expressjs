37ef84eb616ebb61b09a4f31bc7f564764167aa4
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2023-11-19 03:46:39 +0600

    Added user controller for creating a user:

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import validateResource from "../middleware/validateResource";
+import { createUserSchema } from "../schema/user.schema";
+import { createUserHandler } from "../controller/user.controller";
 
 const router = express.Router();
 
 // Create a new user (POST)
-router.post("/api/v1/users", (req, res) => res.sendStatus(200));
+router.post(
+  "/api/v1/users",
+  validateResource(createUserSchema),
+  createUserHandler
+);
 
 export default router;
